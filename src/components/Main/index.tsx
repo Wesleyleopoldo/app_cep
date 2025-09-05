86de8b7d38d438160e67e3e0461406279ee9695c
Your Name <you@example.com>
2025-09-05 20:36:24 -0300

    development(Busca cep): Adiciona os resultados da requisição dentro de uma View
    Signed-off-by: Your Name <you@example.com>

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,18 @@
 import { useRef, useState } from "react";
-import { Text, View, TouchableOpacity, SafeAreaView, TextInput } from "react-native";
+import { Text, View, TouchableOpacity, SafeAreaView, TextInput, Keyboard } from "react-native";
 import { s } from "./styles"
 import Button from "../Button";
-import api from "../../services/api.ts";
+import api from "../../services/api";
 
 export default function Main() {
     const [ cep, setCep ] = useState("");
     const inputRef = useRef<TextInput>(null);
+    const [cepUser, setCepUser] = useState<any>(null);
 
     function limpar() {
         setCep("");
         inputRef.current?.focus();
+        setCepUser(null);
     }
 
     async function buscar() {
@@ -23,6 +25,9 @@ export default function Main() {
         try {
             const response = await api.get(`/ws/${cep}/json/`);
             console.log(response.data);
+            setCep("");
+            setCepUser(response.data);
+            Keyboard.dismiss();
         } catch(error) {
             console.error(error);
         }
@@ -48,6 +53,16 @@ export default function Main() {
                 />
             <Button onPressFind={() => buscar()} onPressClear={() => limpar()}/>
             </View>
+
+            { cepUser &&
+                <View style={s.dadosContainer}>
+                    <Text style={s.dados}>Cep: {cepUser.cep}</Text>
+                    <Text style={s.dados}>Logradouro: {cepUser.logradouro}</Text>
+                    <Text style={s.dados}>Bairro: {cepUser.bairro}</Text>
+                    <Text style={s.dados}>Cidade: {cepUser.localidade}</Text>
+                    <Text style={s.dados}>Estado: {cepUser.uf}</Text>
+                </View>
+            }
 
         </SafeAreaView>
     )
